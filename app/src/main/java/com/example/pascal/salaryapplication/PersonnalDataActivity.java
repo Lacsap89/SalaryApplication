d2c8f90326fe8033f159d94bccb9ff0e96e1ba92
Yannick <yok_9@hotmail.com>
2015-10-29 18:48:21 +0100

    Création des Drawable

--- app/src/main/java/com/example/pascal/salaryapplication/PersonnalDataActivity.java
+++ app/src/main/java/com/example/pascal/salaryapplication/PersonnalDataActivity.java
@@ -17,7 +17,7 @@ public class PersonnalDataActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_personnal_data, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
