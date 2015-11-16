9fcb55616b8a2ecdbe84daa36a71ffc9ca9d0a06
Yannick <yok_9@hotmail.com>
2015-11-16 23:06:25 +0100

    From Login to Personal data (java).
    Pas fini, mais fatigué.

--- app/src/main/java/com/example/pascal/salaryapplication/App/Login.java
+++ app/src/main/java/com/example/pascal/salaryapplication/App/Login.java
@@ -1,11 +1,14 @@
-package com.example.pascal.salaryapplication;
+package com.example.pascal.salaryapplication.App;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.example.pascal.salaryapplication.R;
 
 public class Login extends AppCompatActivity {
 
@@ -15,15 +18,10 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    public void gotoHome(View view) {
+        Intent intent = new Intent(this,PersonnalDataActivity.class);
+        startActivity(intent);
+    }
 }
